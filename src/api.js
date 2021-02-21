f76ef18bbd472f05aeae301745be08082a1ddeb8
Kurt Emch <kurtemch@gmail.com>
2021-02-21 14:49:28 -0500

    update api url

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV === 'development';
-const API_URL = dev? 'http://localhost:1337':'http://3.135.235.251:1337';
+const API_URL = dev? 'http://localhost:1337':'https://kitchen.pixelclad.com';
 export default API_URL;
